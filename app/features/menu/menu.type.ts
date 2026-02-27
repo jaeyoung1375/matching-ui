2c8358dfb0db4afdc6902d145d16d466eaeddd53
jaeyoung <jaeyoung1375@naver.com>
2026-02-27 20:49:32 +0900

    불필요한 파일 제거

--- app/features/menu/menu.type.ts
+++ app/features/menu/menu.type.ts
@@ -1,13 +1,13 @@
 export type Menu = {
-  menuId: string;
-  menuNm: string;
-  menuUrl: string;
-  menuLevel: number;
-  menuOrd?: string;
-  upMenuId?: string;
-  topYn: string;
-  useYn: string;
-  regId?: string;
-  regDt?: string;
+  menuId: string; // 메뉴아이디
+  menuNm: string; // 메뉴명
+  menuUrl: string; // 메뉴URL
+  menuLevel: number; // 메뉴레벨
+  menuOrd?: string; // 메뉴정렬순서
+  upMenuId?: string; // 상위메뉴아이디
+  topYn: string; // 상위여부
+  useYn: string; // 사용여부
+  regId?: string; // 등록자
+  regDt?: string; // 등록일자
   children: Menu[];
 };
