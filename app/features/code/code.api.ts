4d3508a53d6bafad4bce268b708e3042fcc581d3
jaeyoung <jaeyoung1375@naver.com>
2026-03-14 20:11:08 +0900

    멀티공통코드 조회 서비스 추가

---
+++ app/features/code/code.api.ts
@@ -0,0 +1,25 @@
+import { get } from "@/app/util/AxiosUtil";
+import { codeList } from "./code.type";
+
+/**
+ * 공통 코드 목록 조회
+ *
+ * @param comCdIds - 조회할 공통 코드 ID 목록
+ * @returns codeList  - 공통 코드 ID를 key로 하는 코드 목록 Map
+ *
+ * @example
+ * const codes = await fetchCodeList([
+ *   "RECRUIT_TYPE_CD",
+ *   "PROGRESS_TYPE_CD"
+ * ]);
+ *
+ * codes.RECRUIT_TYPE_CD
+ * codes.PROGRESS_TYPE_CD
+ */
+export const fetchCodeList = async (comCdIds: string[]) => {
+  const res = await get<codeList>("/api/v1/codes", {
+    params: { comCdIds },
+  });
+
+  return res;
+};
